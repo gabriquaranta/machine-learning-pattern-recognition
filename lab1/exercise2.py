93e999edcfcbacef184a35cdae5bed365bd84f3a
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-03 15:53:26 +0100

    pip install black formatter && ex2 start

---
+++ lab1/exercise2.py
@@ -0,0 +1,7 @@
+# open file
+with open(
+    "/home/gabri/Polito/ii - Magistrale/0-repos/MachineLearningPatternRecognition/lab1/transports.txt"
+) as inp:
+    inputlines = inp.readlines()
+
+# inputlines list of strings
