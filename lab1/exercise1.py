e8e6407dba1a0b524574cc3547dd6e5d7a2f6a67
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-03 15:53:03 +0100

    pip install black formatter && ex2 start

--- lab1/exercise1.py
+++ lab1/exercise1.py
@@ -1,55 +1,59 @@
-#open file and read to list of strings
-with open("/home/gabri/Polito/ii - Magistrale/0-repos/MachineLearningPatternRecognition/lab1/score.txt") as inputfile:
-    filecontent=inputfile.readlines()
+# open file and read to list of strings
+with open(
+    "/home/gabri/Polito/ii - Magistrale/0-repos/MachineLearningPatternRecognition/lab1/score.txt"
+) as inputfile:
+    filecontent = inputfile.readlines()
 
-#split each string in the list to a list of strings
-fields=map(lambda s:s.split(" "),filecontent)
+# split each string in the list to a list of strings
+fields = map(lambda s: s.split(" "), filecontent)
 
-#list of [player_n player_s country score ...]
-playerscores=list(fields)
-ps=[]
-cs=[]
+# list of [player_n player_s country score ...]
+playerscores = list(fields)
+ps = []
+cs = []
 
 # generates two list of list:
 #   ps = list of [playername, playersocre] one per player
 #   cs = list of [country, playerscore] one per player
 for player in playerscores:
-    name=player[0]+" "+player[1]
-    country=player[2]
+    name = player[0] + " " + player[1]
+    country = player[2]
 
-    maxi=0
-    mini=200
-    for i in range(3,8):
-        tmp=float(player[i])
-        if tmp>maxi: maxi=i
-        if tmp<=mini: mini=i
-        else: player[i]=tmp
-    player[maxi]=0.0
-    player[mini]=0.0
+    maxi = 0
+    mini = 200
+    for i in range(3, 8):
+        tmp = float(player[i])
+        if tmp > maxi:
+            maxi = i
+        if tmp <= mini:
+            mini = i
+        else:
+            player[i] = tmp
+    player[maxi] = 0.0
+    player[mini] = 0.0
 
-    score=0
-    for i in range(3,8): score+=player[i]
+    score = 0
+    for i in range(3, 8):
+        score += player[i]
 
-    ps.append([name,score])
-    cs.append([country,score])
+    ps.append([name, score])
+    cs.append([country, score])
 
-#sort ps and print podium aka first 3
+# sort ps and print podium aka first 3
 print()
 print("podium:")
-ps.sort(key=lambda x:x[1],reverse=True)
-for player in ps[:3]: 
-    print(player[0]+": "+str(player[1]))
+ps.sort(key=lambda x: x[1], reverse=True)
+for player in ps[:3]:
+    print(player[0] + ": " + str(player[1]))
 
 # calc total for each country and print max one
 print()
 print("best country:")
 for cstuple in cs:
     for cstuple2 in cs:
-        if cstuple[0]==cstuple2[0] and cstuple[1]!=cstuple2[1]:
-            cstuple[1]+=cstuple2[1]
-            cstuple2[1]=0.0
-cs.sort(key=lambda x:x[1],reverse=True)
-print(cs[0][0]+": "+str(cs[0][1]))
+        if cstuple[0] == cstuple2[0] and cstuple[1] != cstuple2[1]:
+            cstuple[1] += cstuple2[1]
+            cstuple2[1] = 0.0
+cs.sort(key=lambda x: x[1], reverse=True)
+print(cs[0][0] + ": " + str(cs[0][1]))
 print()
-
-
