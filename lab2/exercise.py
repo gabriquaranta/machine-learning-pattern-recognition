fd1990abcc500860e2c5cd652146f9bac4d2e683
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-06 12:20:08 +0100

    iris dtset attributes per class histograms

--- lab2/exercise.py
+++ lab2/exercise.py
@@ -9,24 +9,37 @@ with open(
 
 # Iris setosa will be indicated with value 0, iris versicolor
 # with value 1 and iris virginica with value 2.
-labels = []
+labels = np.empty(
+    shape=150, dtype=object
+)  # dtype is object for variable length strings
 
 matrix = np.zeros(shape=(150, 4))
+
+attributes = {
+    0: "sepal length (cm)",
+    1: "sepal width (cm)",
+    2: "petal length (cm)",
+    3: "petal width (cm)",
+}
 
 r = 0
 for line in lines:
     fields = line.split(",")
-    labels.append(fields[4])
+    tag = fields[4][:-1]  # remove "\n"
+    labels[r] = tag
     matrix[r] = fields[:4]
     r += 1
 
-# print(matrix)
+# attributo histogram per attribute per class
 for i in range(4):
     plt.figure(i)
-    ms = ma.masked_where(labels == "Iris-setosa", matrix)
-    mv = ma.masked_where(labels == "Iris-versicolor", matrix)
-    mvv = ma.masked_where(labels == "Iris-virginica", matrix)
-    plt.hist(ms[i : i + 1])
-    plt.hist(mv[i : i + 1])
-    plt.hist(mvv[i : i + 1])
+    # matrix[:,i] gets all rows of column i
+    ms = ma.masked_where(labels == "Iris-setosa", matrix[:, i])
+    mv = ma.masked_where(labels == "Iris-versicolor", matrix[:, i])
+    mvv = ma.masked_where(labels == "Iris-virginica", matrix[:, i])
+    plt.title(attributes[i])
+    plt.hist(ms, color="r", alpha=0.4, label="setosa")
+    plt.hist(mv, color="g", alpha=0.4, label="vernicolor")
+    plt.hist(mvv, color="b", alpha=0.4, label="virginica")
+    plt.legend(loc="upper right")
     plt.show()
