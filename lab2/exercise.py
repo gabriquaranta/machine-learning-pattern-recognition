d152531ab2adb16fbd59a5cf34cd0ed77c88b3b3
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-08 23:38:43 +0100

    graphs with attributes centering using mean

--- lab2/exercise.py
+++ lab2/exercise.py
@@ -22,7 +22,7 @@ def attributesHists():
         # plt.show()
 
 
-def scatterplots():
+def scatterPlots():
     figure, axis = plt.subplots(4, 4)
     figure.set_figheight(20)
     figure.set_figwidth(20)
@@ -53,7 +53,51 @@ def scatterplots():
                 axis[i, j].hist(mvi, color="g", alpha=0.4, label="vernicolor")
                 axis[i, j].hist(mvvi, color="b", alpha=0.4, label="virginica")
                 axis[i, j].legend(loc="upper right")
-    plt.savefig("lab2/plots/attributes_pairplots")
+    plt.savefig("lab2/plots/attributes_pairplot")
+    # plt.show()
+
+
+def scatterPlotsCentered():
+    figure, axis = plt.subplots(4, 4)
+    figure.set_figheight(20)
+    figure.set_figwidth(20)
+    for i in range(4):  # attribute
+        for j in range(4):  # other attributes
+            # attributo i
+
+            msi = ma.masked_where(labels == "Iris-setosa", matrix[:, i])
+            mvi = ma.masked_where(labels == "Iris-versicolor", matrix[:, i])
+            mvvi = ma.masked_where(labels == "Iris-virginica", matrix[:, i])
+            # attributo j
+            msj = ma.masked_where(labels == "Iris-setosa", matrix[:, j])
+            mvj = ma.masked_where(labels == "Iris-versicolor", matrix[:, j])
+            mvvj = ma.masked_where(labels == "Iris-virginica", matrix[:, j])
+            # centering
+            msi = msi - msi.mean()
+            mvi = mvi - mvi.mean()
+            mvvi = mvvi - mvvi.mean()
+            msj = msj - msj.mean()
+            mvj = mvj - mvj.mean()
+            mvvj = mvvj - mvvj.mean()
+
+            if i != j:
+                # plt.figure()
+                axis[i, j]
+                axis[i, j].set_title(attributes[i] + "-" + attributes[j] + " (cm)")
+                axis[i, j].set(xlabel=attributes[i], ylabel=attributes[j])
+                # axis[i, j].xlabel(attributes[i])
+                # axis[i, j].ylabel(attributes[j])
+                axis[i, j].scatter(msi, msj, color="r", alpha=0.4, label="setosa")
+                axis[i, j].scatter(mvi, mvj, color="g", alpha=0.4, label="vernicolor")
+                axis[i, j].scatter(mvvi, mvvj, color="b", alpha=0.4, label="virginica")
+                axis[i, j].legend(loc="upper right")
+
+            else:
+                axis[i, j].hist(msi, color="r", alpha=0.4, label="setosa")
+                axis[i, j].hist(mvi, color="g", alpha=0.4, label="vernicolor")
+                axis[i, j].hist(mvvi, color="b", alpha=0.4, label="virginica")
+                axis[i, j].legend(loc="upper right")
+    plt.savefig("lab2/plots/centered_attributes_pairplot")
     # plt.show()
 
 
@@ -86,4 +130,8 @@ for line in lines:
 
 attributesHists()
 
-scatterplots()
+scatterPlots()
+
+scatterPlotsCentered()
+
+# data centering
